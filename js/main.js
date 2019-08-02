1ebb3ba62dde9cf4e216f1383b0f1a7c3fbbd0d2
snsaleh1 <snsaleh11@gmail.com>
2019-08-02 17:29:57 -0500

    started event listeners

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
-//Rep Your City Game
-/*----- constants -----*/
-const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3');
-const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
+// //Rep Your City Game
+// /*----- constants -----*/
+// const studentCards = [{
+//     name: "Caleb Saleh",
+//     city: "atx",
+//     cardImage: "images/atxcalebsaleh.png"
+// }, {
+//     name: "David Bae",
+//     city: "atx",
+//     cardImage: URL(https://i.imgur.com/8AghMPJ.png)
+// },  {
+//     name: "",
+//     city: "a",
+//     cardImage: ""
 
-/*----- app's state (variables) -----*/
-let cityCounts, timer, winner;
+// }]
+// const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3');
+// const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
 
-/*----- cached element references -----*/
+// /*----- app's state (variables) -----*/
+// let cityCounts, timer, winner;
+
+// /*----- cached element references -----*/
 
 
-/*----- event listeners -----*/
-document.querySelector('startGame')
-  .addEventListener('click', startGame);
+// /*----- event listeners -----*/
+// document.querySelector('startGame')
+//   .addEventListener('click', startGame);
 
-/*----- functions -----*/
+// /*----- functions -----*/
+
+// function dealStudent() {
+//     document.querySelector('dealCard')
+//     .addEventListener('click', function(evt){
+//         //random function, then save 
+//     }) 
+// }
+
+// // let ranNum = Math.random() * 75
+// // let obj = tudentCards[ranNum]
+
+// // if(input === )
+
+
+let button = document.querySelector('.Sf');
+
+button.addEventListener('click', function(evt){
+    let input = evt.srcElement.className
+    // console.log(typeof input)
+    if(input === "Sf"){
+        console.log("match")
+    } else {
+        console.log("no match")
+    }
+})
