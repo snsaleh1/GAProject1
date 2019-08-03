c86b6027f6899475c675de81a24233406ef97b31
snsaleh1 <snsaleh11@gmail.com>
2019-08-03 14:26:56 -0500

    added to readme and began dealStudent function

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # GAProject1
-I'm really just tryna live, man.
+Pseudocode – Rep your city!
+
+-	When the game loads:
+
+Player will click on the “Let’s do this” button to begin the game 60 second timer will begin.
+    
+First student card will appear in the middle of the screen
+    
+Random card will be selected from the object list (students) using a splice function so that the student card does not appear again.
+
+-   Player objective:
+
+Player will match the student with the city in which he/she is a part of the cohort before the timer hits 0.
+
+If the city is correct, the student card will bounce to that city and the “counter” attached to that city will go up by 1.
+
+Otherwise if the city clicked is not correct the card will be discarded, and a sound will emit
+-	Winner
+
+A player wins when they’ve matched 10 correct students to any one of the 4 cities before the timer hits 0
+
+If the player runs out of time then the game shuts down and a loss message appears.
--- js/main.js
+++ js/main.js
@@ -1,55 +1,140 @@
-// //Rep Your City Game
-// /*----- constants -----*/
-// const studentCards = [{
-//     name: "Caleb Saleh",
-//     city: "atx",
-//     cardImage: "images/atxcalebsaleh.png"
-// }, {
-//     name: "David Bae",
-//     city: "atx",
-//     cardImage: URL(https://i.imgur.com/8AghMPJ.png)
-// },  {
-//     name: "",
-//     city: "a",
-//     cardImage: ""
+//Rep Your City Game
+/*----- constants -----*/
+const studentCards = [{
+    name: "Caleb Saleh",
+    city: "atx",
+    cardImage: "images/atxcalebsaleh.png"
+}, {
+    name: "David Bae",
+    city: "atx",
+    cardImage: "images/atxdavidbae.png"
+},  {
+    name: "Chighali Mouknass",
+    city: "atx",
+    cardImage: "images/atxchighalimouknass.png"
+},
+    {
+    name: "Dev Govindji",
+    city: "atx",
+    cardImage: "image/atxdevgovindji.png"
+}, 
+    {
+    name: "Heather Etheridge",
+    city: "atx",
+    cardImage: "image/atxheatheretheridge.png"
+},
+    {
+    name: "Jack Ground",
+    city: "atx",
+    cardImage: "image/atxjackground.png"
+},
+    {
+    name: "Beverly Osoria",
+    city: "atx",
+    cardImage: "image/atxbeverlyosoria"
+},
+    {
 
-// }]
-// const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3');
-// const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
+},
+    {
 
-// /*----- app's state (variables) -----*/
-// let cityCounts, timer, winner;
+},
+    {
 
-// /*----- cached element references -----*/
+},
+    {
+
+},
+    {
+
+},
+    {
+
+},
+    {
+
+},
+    {
+
+},
+    {
+
+},
+]
+const beepAudio = new Audio('http://soundbible.com/mp3/Robot_blip-Marianne_Gagnon-120342607.mp3');
+const shootAudio = new Audio('http://soundbible.com/mp3/shooting_star-Mike_Koenig-1132888100.mp3');
+
+/*----- app's state (variables) -----*/
+let cityCounts, timer, winner;
+
+/*----- cached element references -----*/
 
 
-// /*----- event listeners -----*/
-// document.querySelector('startGame')
-//   .addEventListener('click', startGame);
-
-// /*----- functions -----*/
-
-// function dealStudent() {
-//     document.querySelector('dealCard')
-//     .addEventListener('click', function(evt){
-//         //random function, then save 
-//     }) 
-// }
-
-// // let ranNum = Math.random() * 75
-// // let obj = tudentCards[ranNum]
-
-// // if(input === )
+/*----- event listeners -----*/
 
 
-let button = document.querySelector('.Sf');
+/*----- functions -----*/
+
+function dealStudent() {
+    document.querySelector('dealCard')
+    .addEventListener('click', function(evt){
+        //random function, then save 
+    }) 
+}
+
+console.log(Object.keys(studentCards));
+
+// let ranNum = Math.random() * 75
+// let obj = tudentCards[ranNum]
+
+// if(input === )
+
+
+let button = document.querySelector('.sf');
 
 button.addEventListener('click', function(evt){
     let input = evt.srcElement.className
     // console.log(typeof input)
-    if(input === "Sf"){
+    if(input === "sf"){
+        console.log("match")
+    } else {
+        console.log("no match")
+}
+})
+
+let butsm = document.querySelector('.sm');
+
+butsm.addEventListener('click', function(evt){
+    let input = evt.srcElement.className
+    // console.log(typeof input)
+    if(input === "sm"){
         console.log("match")
     } else {
         console.log("no match")
     }
 })
+
+let butdal = document.querySelector('.dal');
+
+butdal.addEventListener('click', function(evt){
+    let input = evt.srcElement.className
+    // console.log(typeof input)
+    if(input === "dal"){
+        console.log("match")
+    } else {
+        console.log("no match")
+    }
+})
+
+let butatx = document.querySelector('.atx');
+butatx.addEventListener('click', function(evt){
+    let input = evt.srcElement.className
+    // console.log(typeof input)
+    if(input === "atx"){
+        console.log("match")
+    } else {
+        console.log("no match")
+    }
+});
+
+var rs = Math.floor()
